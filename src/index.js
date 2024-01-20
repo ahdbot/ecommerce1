8491223622312ec7b5ccbc172da1ba80b20d03ca
ahdbot <ahdbot3@gmail.com>
2024-01-20 18:35:19 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+window.bootstrap = require("bootstrap/dist/js/bootstrap.bundle.js");
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/js/all.min";
+import "./css/style.css";
+
+document.querySelectorAll('[data-bs-toggle="tooltip"]').forEach(item => new bootstrap.Tooltip(item))
+
+console.log("أهلا بكم في متجر عربي")
+console.log("أهلا بكم في متجر عربي")
