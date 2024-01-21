e517b49d711cba84fedefc3293c5a885724fc7da
ahdbot <ahdbot3@gmail.com>
2024-01-21 17:06:57 +0300

    most sold product

--- src/index.js
+++ src/index.js
@@ -4,6 +4,13 @@ import "@fortawesome/fontawesome-free/js/all.min";
 import "./css/style.css";
 
 document.querySelectorAll('[data-bs-toggle="tooltip"]').forEach(item => new bootstrap.Tooltip(item))
+document.querySelectorAll('.add-to-cart-btn').forEach(item => {
+item.addEventListener("click",() => {
+
+alert('اضيف المنتج الى عربة الشراء');
+})
+
+})
 
 console.log("أهلا بكم في متجر عربي")
 console.log("أهلا بكم في متجر عربي")
