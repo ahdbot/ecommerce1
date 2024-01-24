e8d3f39ab6079400d2887c6396f74ecb86df123e
ahdbot <ahdbot3@gmail.com>
2024-01-24 17:37:05 +0300

    check out page done

--- src/index.js
+++ src/index.js
@@ -31,6 +31,55 @@ document.querySelectorAll('.size-option input[type="radio"]').forEach(item => {
     
     })
 })
+
+// حساب سعر اجمالي المنتج
+document.querySelectorAll('[data-product-quantity]').forEach(item => {
+    item.addEventListener('change',() => {
+    const newQuantity = item.value;
+    const parent = item.closest('[data-product-info]')
+    const pricePerUnit =  parent.getAttribute('data-product-price');
+    const totalPriceForProduct = newQuantity * pricePerUnit;
+    parent.querySelector('.total-price-for-product').innerHTML = totalPriceForProduct + "$"
+        
+
+    let totalpriceForAllProduct = 0;
+    document.querySelectorAll('[data-product-info]').forEach(product => {
+             const pricePerUnit = product.getAttribute('data-product-price');
+             const quantity = product.querySelector('[data-product-quantity]').value
+             const totalpriceForProduct = pricePerUnit * quantity
+
+             totalpriceForAllProduct = totalpriceForAllProduct + totalpriceForProduct;
+    })
+
+    document.getElementById('total-price-for-all-product').innerHTML = totalpriceForAllProduct + '$'
+     })
+
+})
+
+document.querySelectorAll('[data-remove-from-card]').forEach(item => {
+item.addEventListener('click',() => {
+item.closest('[data-product-info]').remove()
+calculateTotalPrice ();
+})
+
+})
+// حساب السعر الاجمالي
+
+function calculateTotalPrice ()
+{
+    let totalpriceForAllProduct = 0;
+    document.querySelectorAll('[data-product-info]').forEach(product => {
+             const pricePerUnit = product.getAttribute('data-product-price');
+             const quantity = product.querySelector('[data-product-quantity]').value
+             const totalpriceForProduct = pricePerUnit * quantity
+    
+             totalpriceForAllProduct = totalpriceForAllProduct + totalpriceForProduct;
+    })
+    
+    document.getElementById('total-price-for-all-product').innerHTML = totalpriceForAllProduct + '$'
+}
+
+document.getElementById("product-quantity")
 document.getElementById("copyright").innerHTML = "جميع الحقوق محفوظة للمتجر سنة " + new Date().getFullYear();
 
 
